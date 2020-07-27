18ede1be019c4544b6c28b492780d0863316d7d4
Marco <maalanis@miners.utep.edu>
2020-07-27 01:28:26 -0600

    working on moving the screens

--- shapeLib/shapedemo.c
+++ shapeLib/shapedemo.c
@@ -4,17 +4,46 @@
 #include "lcddraw.h"
 #include "shape.h"
 #include "p2switches.h"
+#include "abCircle.h"
 
-const AbRect rect10 = {abRectGetBounds, abRectCheck, 10,10};;
+const AbRect rect10 = {abRectGetBounds, abRectCheck, 10,10};
 
 #define GREEN_LED BIT6
 
+AbRectOutline fieldOutline = {
+  abRectOutlineGetBounds, abRectOutlineCheck,
+  {screenWidth/2-10, screenHeight/2-10}
+};
+
+Layer layer1 = {
+  (AbShape *) &circle8,
+  {(screenWidth/2)-5, (screenHeight/2)+10},
+  {0,0}, {0,0},
+  COLOR_YELLOW,
+  0
+};
+
+
+
+Layer fieldLayer = {
+  (AbShape *) &fieldOutline,
+  {(screenWidth/2),screenHeight/2},
+  {0,0}, {0,0},
+  COLOR_RED,
+  &layer1,
+};
+/*
+Layer layer1 = {
+  (AbShape *&cicle8,
+   ((screenW
+};
+*/
 Layer layer0 ={
   (AbShape *)&rect10,
-  {(screenWidth/2)*10, (screenHeight/2)+5},
+  {(screenWidth/2)+10, (screenHeight/2)+5},
   {0,0}, {0,0},
-  COLOR_ORANGE,
-  0,
+  COLOR_BLACK,
+  &fieldLayer,
 };
 void abDrawPos(AbShape *shape, Vec2 *shapeCenter, u_int fg_color, u_int bg_color)
 {
@@ -38,6 +67,9 @@ typedef struct MovLayer_s {
   struct MovLayer_s *next;
 } MovLayer;
 
+MovLayer ml1 = {&layer1, {2,2}, 0};
+MovLayer ml0 = {&layer0, {2,2}, &ml1};
+
 void mlAdvance(MovLayer *ml, Region *fence)
 {
   Vec2 newPos;
@@ -57,9 +89,37 @@ void mlAdvance(MovLayer *ml, Region *fence)
   }
   
 }
+void movLayerDraw(MovLayer *movLayers, Layer *layers)
+{
+  int row, col;
+  MovLayer *movLayer;
 
-MovLayer ml0 = {&layer0, {2,1}, 0};
+  and_sr(8);
+  for(movLayer = movLayers; movLayer; movLayer = movLayer->next) {
+    Region bounds;
+    layerGetBounds(movLayer->layer, &bounds);
+    lcd_setArea(bounds.topLeft.axes[0], bounds.topLeft.axes[1],
+		bounds.botRight.axes[0], bounds.botRight.axes[1]);
+    for(row = bounds.topLeft.axes[1]; row <= bounds.botRight.axes[1]; row++) {
+      for(col = bounds.topLeft.axes[0]; col <= bounds.botRight.axes[0]; col++) {
+	Vec2 pixelPos = {col, row};
+	u_int color = bgColor;
+	Layer *probeLayer;
+	for(probeLayer = layers; probeLayer;
+	    probeLayer = probeLayer->next) {
+	  if(abShapeCheck(probeLayer->abShape, &probeLayer->pos, &pixelPos)) {
+	    color = probeLayer->color;
+	    break;
+	  }
+	}
+	lcd_writeColor(color);
+      }
+    }
+  }
+}
 
+
+u_int bgColor = COLOR_BLUE;
 int redrawScreen = 1;
 
 Region fieldFence;
@@ -69,21 +129,28 @@ Region fieldFence;
 int
 main()
 {
+
+  P1DIR |= GREEN_LED;
+  P1OUT |= GREEN_LED;
   configureClocks();
   lcd_init();
   p2sw_init(1);
   shapeInit();
 
+  shapeInit();
+  
   layerInit(&layer0);
   layerDraw(&layer0);
 
+  
+  
   layerGetBounds(&fieldLayer, &fieldFence);
   Vec2 rectPos = screenCenter;
 
-  clearScreen(COLOR_BLUE);
-  drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
-  shapeInit();
-  drawPixel(screenCenter.axes[1], screenCenter.axes[0], COLOR_BLACK);
+  //clearScreen(COLOR_BLUE);
+   drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
+  //shapeInit();
+  // drawPixel(screenCenter.axes[1], screenCenter.axes[0], COLOR_BLACK);
   //abDrawPos((AbShape*)&rect10, &rectPos, COLOR_ORANGE, COLOR_BLUE);
   enableWDTInterrupts();
   for(;;){
